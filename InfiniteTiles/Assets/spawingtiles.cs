7add7a5ee864d1a09ea5c8ab96cbd6c79bf98324
chanpreet3000 <66767005+chanpreet3000@users.noreply.github.com>
2021-01-04 18:23:02 +0530

    infinitegameobject

---
+++ InfiniteTiles/Assets/spawingtiles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawingtiles : MonoBehaviour
+{
+    public GameObject Ground;
+    public Transform Player;   
+    public int initialtiles = 3;
+    public float tilesize = 50f;
+
+    private Vector3 spawnposition = Vector3.zero;
+
+    private void Update()
+    {
+        if(spawnposition.z - Player.position.z < initialtiles*tilesize)
+        {
+            Instantiate(Ground, spawnposition, Quaternion.identity);
+            spawnposition.z = spawnposition.z + tilesize;
+        }
+    }
+}
